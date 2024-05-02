938c885ed478b1f8fcd4315f93ffb1cfd317d354
cjw102 <cjw102@pitt.edu>
2024-05-02 07:43:42 -0400

    closure methods

--- ch2-1.js
+++ ch2-1.js
@@ -1,3 +1,4 @@
+//from the website https://dev.to/acsreedharreddy/represent-pair-data-structure-using-functions-4ecp
 function pair(m, n)
 {
     return (val) => (val === 0 ? m : n);
@@ -11,4 +12,40 @@ function head(p)
 function tail(p)
 {
     return p(1);
+}
+
+function rational(m,n)
+{
+    return pair(m,n);
+}
+
+function numer(x)
+{
+    return head(x);
+}
+
+function denom(x)
+{
+    return tail(x);
+}
+
+function counter()
+{
+    var count = 0;
+    return () => count++;
+} 
+//var x = counter(); 
+//x()
+
+function foo()
+{
+    var x = 0;
+    var y = 1;
+    return (n) => (n===0 ? x = x+1 : y = 2*y);
+}
+
+
+function um()
+{
+    return 2;
 }
